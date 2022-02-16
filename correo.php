7ba23f6bdd8b419f5b5c578a5cd5af8aee48dee1
mister-rapalino <99707366+mister-rapalino@users.noreply.github.com>
2022-02-15 23:44:54 -0500

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,25 @@
+<?php
+
+ $destinatario = `mister.rapalino@gmail.com`;
+
+ $correo = $_POST[`correo`];
+
+ $mensaje = $_POST[`mensaje`];
+
+ $asunto = $_POST[`asunto`];
+
+
+ $header = "Enviado desde mrapalino.com ";
+
+ $mensajeCompleto = $mensaje . "\nAtentamente: " . $correo;
+
+ mail($destinatario, $asunto, $mensajeCompleto, $header);
+
+ echo "<script> alert("correo enviado exitosamente") </script> ";
+
+
+
+
+
+
+?>
